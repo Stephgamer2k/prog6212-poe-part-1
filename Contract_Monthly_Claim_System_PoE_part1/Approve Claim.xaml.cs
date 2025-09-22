a5fd1a7d404f37789f6fce6efe6cacc403631354
Stephgamer2k <123049227+Stephgamer2k@users.noreply.github.com>
2025-09-22 21:16:05 +0200

    CMCS Added Manage Lecturer and Claim Approval Windows

---
+++ Contract_Monthly_Claim_System_PoE_part1/Approve Claim.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contract_Monthly_Claim_System_PoE_part1
+{
+    /// <summary>
+    /// Interaction logic for Approve_Claim.xaml
+    /// </summary>
+    public partial class Approve_Claim : Window
+    {
+        public Approve_Claim()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Contract_Monthly_Claim_System_PoE_part1/Manage Lecturer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contract_Monthly_Claim_System_PoE_part1
+{
+    /// <summary>
+    /// Interaction logic for Manage_Lecturer.xaml
+    /// </summary>
+    public partial class Manage_Lecturer : Window
+    {
+        public Manage_Lecturer()
+        {
+            InitializeComponent();
+        }
+    }
+}
