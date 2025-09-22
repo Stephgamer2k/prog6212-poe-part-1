6f58493fe985b056317facd6e36e92b38906bbed
Stephgamer2k <123049227+Stephgamer2k@users.noreply.github.com>
2025-09-22 18:35:25 +0200

    CMCS Added Lecturer Dashboard and Claim History Windows

---
+++ Contract_Monthly_Claim_System_PoE_part1/Claim History.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contract_Monthly_Claim_System_PoE_part1
+{
+    /// <summary>
+    /// Interaction logic for Claim_History.xaml
+    /// </summary>
+    public partial class Claim_History : Window
+    {
+        public Claim_History()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Contract_Monthly_Claim_System_PoE_part1/Lecturer Dashboard.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contract_Monthly_Claim_System_PoE_part1
+{
+    /// <summary>
+    /// Interaction logic for Lecturer_Dashboard.xaml
+    /// </summary>
+    public partial class Lecturer_Dashboard : Window
+    {
+        public Lecturer_Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
